1ee64d927c4c03801e82187f7145992863774fae
Malakhova Anna <special.asister@gmail.com>
2020-04-20 20:44:28 +0300

    Add string

---
+++ String/String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            String str = Console.ReadLine();
+            String str2 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                {
+                    str2 += str[i];
+                }
+
+            }
+            Console.WriteLine(str2);
+        }
+    }
+
+}
